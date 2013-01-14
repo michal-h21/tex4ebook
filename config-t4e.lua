4d5cccb16f3d06f3c6d1593042021d3b7188a0b6
Michal Hoftich <michal.h21@gmail.com>
2013-01-14 17:41:01 +0100

    Added font config

---
+++ config-t4e.lua
@@ -0,0 +1,19 @@
+-- Config file for tex4ebook
+
+-- Font config
+
+Font{
+  name   = "cmr",
+  file   = "lmroman10-regular.otf",
+  family = "Latin Modern",
+  weight = "normal",
+  style  = "normal"
+}
+
+Font{
+  name   = "cmmi",
+  file   = "lmroman10-italic.otf",
+  family = "Latin Modern",
+  weight = "normal",
+  style  = "italic"
+}
